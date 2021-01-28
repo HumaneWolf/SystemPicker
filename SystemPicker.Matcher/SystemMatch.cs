cc6c616c8e5fcf27bb833fe9c5fd5776629985ab
Kristian H.T <kristian@kristianht.com>
2021-01-28 11:45:11 +0100

    Found more specific inara search link, for system only.

--- SystemPicker.Matcher/SystemMatch.cs
+++ SystemPicker.Matcher/SystemMatch.cs
@@ -9,7 +9,7 @@ namespace SystemPicker.Matcher
         
         public string EDDBUrl => $"https://eddb.io/system/name/{HttpUtility.HtmlEncode(Name)}";
         public string EDSMUrl => $"https://www.edsm.net/en/system/id/_/name/{HttpUtility.HtmlEncode(Name)}";
-        public string InaraUrl => $"https://inara.cz/search/?searchglobal={HttpUtility.HtmlEncode(Name)}";
+        public string InaraUrl => $"https://inara.cz/galaxy-starsystem/?search={HttpUtility.HtmlEncode(Name)}";
         public string SpanshUrl => $"https://spansh.co.uk/system/{HttpUtility.HtmlEncode(Id64)}";
 
         public SystemMatch(string name, long id64)
