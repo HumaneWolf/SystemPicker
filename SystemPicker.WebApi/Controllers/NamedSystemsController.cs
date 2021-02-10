87a71888ca300a97e61aa50b65f209801b451199
Kristian H.T <kristian@kristianht.com>
2021-02-10 21:20:47 +0100

    Add a paginated named systems endpoint.

---
+++ SystemPicker.WebApi/Controllers/NamedSystemsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SystemPicker.Matcher.Finders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SystemPicker.WebApi.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class NamedSystemsController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(List<string>), 200)]
+        public IActionResult Get(int page = 1, int pageSize = 500)
+        {
+            if (pageSize > 5000) pageSize = 5000;
+            var result = NamedSystemFinder.Systems
+                .Skip(pageSize * (page - 1)).Take(pageSize).ToList();
+            return Ok(result);
+        }
+    }
+}
